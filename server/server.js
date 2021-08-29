bd0f21d822e221fa775412966ac92d55adaf263e
Elius Etienne <eetienn02@gmail.com>
2021-08-28 22:59:59 -0400

    mutation added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
@@ -13,7 +14,8 @@ app.use(express.json());
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const startServer = async () => {
